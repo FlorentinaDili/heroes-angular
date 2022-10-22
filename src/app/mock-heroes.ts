19a09698d072e524971d8c79f03d1534b10b8c3a
FlorentinaDili <73610425+FlorentinaDili@users.noreply.github.com>
2022-10-23 01:56:18 +0200

    Finishing the second part

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero [] =[
+    {id:1, name:'Vesa'},
+    {id:2, name:'Vjosa'},
+    {id:3, name:'Noah'},
+    {id:4, name:'Florentina'},
+    {id:5, name:'Anna'},
+    {id:6, name:'Jack'},
+    {id:7, name:'Tom'},
+    {id:8, name:'Tony'},
+];
